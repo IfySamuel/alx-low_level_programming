1001a08e24ccb614f7e98b87900a1d6ebd352f9e
IfySamuelDania <anahimisamuel@yahoo.com>
2022-09-17 17:08:37 -0700

    cyst

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,25 @@
 #include "main.h"
-#include "6-abs.c"
 #include <stdio.h>
-
 /**
- * print_to_98 - prints numbers
- *
- * @n: integer to be passed
- *
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
  */
 
 void print_to_98(int n)
 {
-	int i;
 
-	if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-	} else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
-	printf("\n");
+while (n < 98)
+{
+	printf("%i, ", n);
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98");
+putchar('\n');
 }
