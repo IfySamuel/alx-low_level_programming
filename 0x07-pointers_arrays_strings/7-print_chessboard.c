05245016596a4daf96ab286905e1cb53fd5fc1c1
IfySamuelDania <anahimisamuel@yahoo.com>
2022-09-26 17:44:04 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,14 +6,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+unsigned int i, m = 0;
 
-for (i = 0; i < 8; i++)
-{
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 64; i++)
 	{
-		_putchar(a[i][j]);
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
 	}
 	_putchar('\n');
 }
-}
+
